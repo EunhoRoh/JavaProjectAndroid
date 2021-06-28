e5603b4089c9d20aedb7fd58a50592eb3a805356
EUNHOROH <deu04189@gmail.com>
2021-06-28 14:48:00 +0900

    java android project 1 (revise2)

---
+++ src/main/java/MenuShow.java
@@ -0,0 +1,9 @@
+public class MenuShow {
+    System.out.println("---menu---");
+    System.out.println("1. 조회");
+    System.out.println("2. 추가");
+    System.out.println("3. 수정");
+    System.out.println("4. 삭제");
+    System.out.println("5. 이름검색");
+    System.out.println("0. 종료");
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,5 +7,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class main {
+    int id;
+    char name;
+    int regiDate;
+    int split;
+    char part;
+    int numOfSets;
+    int reps;
+    int weight;
+    char exercise;
+
+    public char getName() {
+        return name;
+    }
+
+    public void setName(char name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRegiDate() {
+        return regiDate;
+    }
+
+    public void setRegiDate(int regiDate) {
+        this.regiDate = regiDate;
+    }
+
+    public int getSplit() {
+        return split;
+    }
+
+    public void setSplit(int split) {
+        this.split = split;
+    }
+
+    public char getPart() {
+        return part;
+    }
+
+    public void setPart(char part) {
+        this.part = part;
+    }
+
+    public int getNumOfSets() {
+        return numOfSets;
+    }
+
+    public void setNumOfSets(int numOfSets) {
+        this.numOfSets = numOfSets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public char getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(char exercise) {
+        this.exercise = exercise;
+    }
+
+
+
 
 }
