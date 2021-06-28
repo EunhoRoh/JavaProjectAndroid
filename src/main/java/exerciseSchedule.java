247c7793cf98e0fc4cd2d7a86844ed008fd1204d
EUNHOROH <deu04189@gmail.com>
2021-06-28 14:13:41 +0900

    java android project 1 (revise1)

--- src/main/java/exerciseSchedule.java
+++ src/main/java/exerciseSchedule.java
@@ -2,4 +2,5 @@ public class exerciseSchedule {
     int id;
     char name;
 
+
 }
---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+
+}
